fc722e56240cbf0df8652b2c84d58184a23065d6
Imipenem <philipp.ehmele@yahoo.de>
2019-03-04 10:28:15 +0100

    Added insertAt()
    - insertAt() inserts a node at a given index (or at the list´s end if the index is outOfBonds --> greater than lists´s size - 1

--- src/main/kotlin/doublelinkedlist/doubleLinkedList.kt
+++ src/main/kotlin/doublelinkedlist/doubleLinkedList.kt
@@ -1,16 +1,13 @@
 package doublelinkedlist
 
-import java.lang.IllegalStateException
-
 class DoubleLinkedList<T>(var head: Node<T>?, var tail: Node<T>?) {
 
     /**
      * Insert the node at the head of the linkedList (if there´s already a head node, the node will get it´s follower
      */
     fun insertFirst(node: Node<T>?) {
-        when {
-            size() > 1 -> throw IllegalStateException("Cannot invoke insertFirst() on lists with size greater than 1")
-            head == null -> {
+        when (head) {
+            null -> {
                 head = node
                 tail = head
             }
@@ -26,7 +23,7 @@ class DoubleLinkedList<T>(var head: Node<T>?, var tail: Node<T>?) {
     /**
      * This function returns the size of the list
      */
-    fun size(): Int {
+    private fun size(): Int {
         var currentNode = head
         var size = 0
         while (currentNode != null) {
@@ -39,10 +36,32 @@ class DoubleLinkedList<T>(var head: Node<T>?, var tail: Node<T>?) {
     /**
      * This function inserts a node at the end of the list
      */
-    fun insertLast(node:Node<T>?) {
+    private fun insertLast(node: Node<T>?) {
         tail?.follower = node
         node?.precursor = tail
         tail = node
+    }
+
+    /**
+     * This function inserts a node at a given position (or at the end of index is outOfBonds)
+     */
+    fun insertAt(node: Node<T>?, index: Int) {
+        when {
+            index == 0 -> insertFirst(node) //doesn't work on lists size > 1!!!!
+            index > size() - 1 -> insertLast(node)
+            else -> {
+                var currentNode = head
+                var currentIndex = 0
+                while (currentIndex != index) {
+                    currentNode = currentNode?.follower
+                    currentIndex++
+                }
+                node?.follower = currentNode
+                node?.precursor = currentNode?.precursor
+                node?.follower?.precursor = node
+                node?.precursor?.follower = node
+            }
+        }
     }
 }
 
@@ -50,20 +69,19 @@ class DoubleLinkedList<T>(var head: Node<T>?, var tail: Node<T>?) {
  * Little test main()
  */
 fun main() {
-    val list = DoubleLinkedList<Int>(null,null)
+    val list = DoubleLinkedList<Int>(null, null)
     val node = Node(null, null, 1)
     val node2 = Node(null, null, 2)
     val node3 = Node(null, null, 3)
     val node4 = Node(null, null, 4)
     val node5 = Node(null, null, 5)
     list.insertFirst(node)
-    list.insertFirst(node5)
-    var testNode:Node<Int>? = list.head
-    list.insertLast(node2)
-    list.insertLast(node3)
-    list.insertLast(node4)
-    println(list.size())
-    while(testNode!=null){
+    list.insertAt(node3, 5)
+    list.insertAt(node2, 1)
+    list.insertAt(node4, 2)
+    list.insertAt(node5, 0)
+    var testNode: Node<Int>? = list.head
+    while (testNode != null) {
         println(testNode)
         testNode = testNode.follower
     }
