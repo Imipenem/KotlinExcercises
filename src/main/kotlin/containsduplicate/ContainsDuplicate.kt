a85997ec145c56cf0e5e404d2c03e983f5c164fc
Imipenem <philipp.ehmele@yahoo.de>
2019-03-16 17:32:55 +0100

    ContainsDuplicateII
    - implemented with a list, but this exercise will better work using arrays and their indexed access

--- src/main/kotlin/containsduplicate/ContainsDuplicate.kt
+++ src/main/kotlin/containsduplicate/ContainsDuplicate.kt
@@ -17,6 +17,23 @@ class ContainsDuplicate<T>{
         }
         return false
     }
+
+    //checks wether there are two equal elements and their absoult difference is at most k
+    fun hasDuplicateII(elements: Array<T>, k:Int):Boolean {
+        val map = HashMap<T,Int>()
+        for (e in elements){
+            if (!map.containsKey(e)){
+                map[e] = elements.indexOf(e)
+            }
+            else if (map.containsKey(e) && Math.abs(map[e]!!.minus(elements.lastIndexOf(e))) >= k){
+                return true
+            }
+        }
+        return false
+    }
+
+    //this can also be solved using a var to keep track of the current index (will most likely work better with arrays)
+    
 }
 
 fun main(){
@@ -30,4 +47,11 @@ fun main(){
     println(obj.hasDuplicate2(arrayOf(1,2,3,4,5,6,6,8,9,10)))
     println(obj.hasDuplicate2(arrayOf(0,2,3,4,5,6,7,8,9,0)))
     println(obj.hasDuplicate2(arrayOf(500,2,3,4,5,6,7,8,9,10)))
+
+    println(obj.hasDuplicateII(arrayOf(1,2,3,4,5,6,7,8,9,10),7))
+    println(obj.hasDuplicateII(arrayOf(1,2,3,4,5,6,6,8,9,10),10))
+    println(obj.hasDuplicateII(arrayOf(0,2,3,4,5,6,7,8,9,0),6))
+    println(obj.hasDuplicateII(arrayOf(500,2,3,10,4,5,6,7,8,9,10),7))
+    println(obj.hasDuplicateII(arrayOf(500,2,3,10,4,2,6,7,8,2,10),5))
+    println(obj.hasDuplicateII(arrayOf(500,2,2,10,4,5,6,7,2,9,10),15))
 }
