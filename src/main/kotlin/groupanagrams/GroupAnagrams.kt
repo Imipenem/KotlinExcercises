2d124ae9ff68b9dede60a5fa42632a81e4d217f7
Imipenem <philipp_ehm@protonmail.com>
2019-03-19 22:49:41 +0100

    Group Anagrams Together

---
+++ src/main/kotlin/groupanagrams/GroupAnagrams.kt
@@ -0,0 +1,36 @@
+package groupanagrams
+
+import java.util.*
+
+class GroupAnagrams {
+
+    fun groupAnagrams(array: Array<String>) {
+        val map = HashMap<Int, MutableList<String>>()
+        for (i in 0 until array.size) {
+
+            val word = array[i]
+            val chars = word.toCharArray()
+            Arrays.sort(chars)
+            val newWord = String(chars)
+            val hash = newWord.hashCode() //the hashcode of the new String object
+
+            if (!map.containsKey(hash)) {
+                map[hash] = mutableListOf()
+            }
+
+            map.getValue(hash).add(word)
+        }
+
+        map.forEach { (_, value) ->
+            run {
+                value.forEach { println(it) }
+                println("-------------------")
+            }
+        }
+    }
+}
+
+fun main() {
+    val obj = GroupAnagrams()
+    obj.groupAnagrams(arrayOf("eat", "tea", "tan", "ate", "nat", "bat"))
+}
