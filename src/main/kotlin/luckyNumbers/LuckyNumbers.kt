a9cfc6873e5a43b27156c749461f0ab5fd698615
Imipenem <philipp.ehmele@yahoo.de>
2019-04-14 23:46:08 +0200

    LuckyNumbers

---
+++ src/main/kotlin/luckyNumbers/LuckyNumbers.kt
@@ -0,0 +1,19 @@
+package luckyNumbers
+
+class LuckyNumbers {
+    fun indexLucky(n:String):String{
+        val ind = (1-(Math.pow(2.0,n.length.toDouble())))/(1-2)
+        var result = ""
+        for(i in 0 until n.length){
+            result += if (n[i] == '4'){
+                '0'
+            } else '1'
+        }
+        return "Offset is ${ind.toInt()} + $result (binary)"
+    }
+}
+
+fun main(){
+    val obj = LuckyNumbers()
+    println(obj.indexLucky("477"))
+}
