a7493bd706476344ce23881e1b0fcbee1e1ae3e6
Imipenem <philipp_ehm@protonmail.com>
2019-04-23 14:42:02 +0200

    Minimize String [CODEFORCES]

---
+++ src/main/kotlin/minimizeString/MinimizeString.kt
@@ -0,0 +1,15 @@
+package minimizeString
+
+fun minimize(s:String):String{
+    for((ind, i) in (1 until s.length).withIndex()){
+        if(s[i] < s[ind]){
+            return s.removeRange(ind,ind+1)
+        }
+    }
+    return s.removeRange(s.length-1,s.length)
+}
+
+fun main(args:Array<String>){
+    val n = readLine()
+    println(minimize(readLine()!!))
+}
