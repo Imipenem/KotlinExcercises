f4f8ff3fd42090e404853b01bb30e1696aeaa840
Imipenem <philipp.ehmele@yahoo.de>
2019-04-25 10:00:51 +0200

    CoverPoints[CODEFORCES]

---
+++ src/main/kotlin/coverpoints/CoverPoints.kt
@@ -0,0 +1,26 @@
+package coverpoints
+
+import kotlin.math.max
+
+fun minLengthCoverPoints(): Int {
+    val n = readLine()!!.toInt()
+    var count = 0
+    var s = readLine()!!.split(" ")
+    var maxPointRange = 0
+
+    do {
+        maxPointRange = max(maxPointRange, (s[0].toInt() + s[1].toInt()))
+        if (count + 1 == n) {
+            break
+        }
+        s = readLine()!!.split(" ")
+        count++
+
+    } while (true)
+
+    return maxPointRange
+}
+
+fun main(args: Array<String>) {
+    println(minLengthCoverPoints())
+}
