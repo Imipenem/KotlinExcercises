34cd46bc260ec20cc05ea2126b5e6fd658b0664a
Imipenem <philipp.ehmele@yahoo.de>
2019-04-25 09:05:13 +0200

    Heaters [CODEFORCES]

---
+++ src/main/kotlin/heaters/Heaters.kt
@@ -0,0 +1,43 @@
+package heaters
+
+fun countHeaters(n:Int,v:Int,array:List<String>):Int{
+     var lastHeater = -1
+     var heaters = 0
+     var rightmost = v-1
+    if(rightmost > n-1){
+        rightmost = n-1
+    }
+
+    while(array[rightmost]!="1"){
+        rightmost--
+        if(rightmost==lastHeater){
+            return -1
+        }
+    }
+    lastHeater = rightmost
+    heaters++
+
+    while(lastHeater+(v-1) < n-1){
+        rightmost+=(2*v-1)
+        if(rightmost > n-1){
+            rightmost = n-1
+        }
+
+        while(array[rightmost]!="1"){
+            rightmost--
+            if(rightmost==lastHeater){
+                return -1
+            }
+        }
+        lastHeater = rightmost
+        heaters++
+    }
+    return heaters
+}
+
+fun main(args:Array<String>){
+    val s = readLine()!!.split(" ")
+    val t = readLine()!!.split(" ")
+    println(countHeaters(s[0].toInt(),s[1].toInt(), t))
+
+}
