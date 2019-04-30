cd866d4cafcc4d528c2f3741e391ea491c4d6583
Imipenem <philipp.ehmele@yahoo.de>
2019-04-30 23:22:04 +0200

    Water Buying [CODEFORCES]

---
+++ src/main/kotlin/waterbuying/WaterBuying.kt
@@ -0,0 +1,16 @@
+package waterbuying
+
+
+fun minCost(n:Long, a:Int, b:Int) = if(a <= b/2) n * a else ((n/2)*b + (n%2)*a)
+
+fun main(args:Array<String>){
+    val q = readLine()!!.toInt()
+    var count = 0
+    var s: List<String>
+
+    while(count < q){
+         s = readLine()!!.split(" ")
+        println(minCost(s[0].toLong(),s[1].toInt(),s[2].toInt()))
+        count++
+    }
+}
