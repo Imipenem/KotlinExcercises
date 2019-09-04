66944c9ce7a8990e1b896e37efdad0fe04ae8364
Imipenem <philipp_ehm@protonmail.com>
2019-09-04 21:48:22 +0200

    409. Longest Palindrome: LEETCODE
    - returns the length of the longest palindrom that can be created out of a given string (initial order does not matter)

--- src/main/kotlin/leetcode/longestpalindrom/LongestPalindrom.kt
+++ src/main/kotlin/leetcode/longestpalindrom/LongestPalindrom.kt
@@ -8,7 +8,6 @@ fun longestPalindrome(s: String): Int {
     for (key in map) {
         res += if (key.value % 2 == 0) key.value else key.value - 1
     }
-
     return if (res < s.length) res + 1 else res
 }
 
